efb6e2ef471246cbd9ae3d2c3f866b08426e86da
Sondos Mohamed <sondos_ali_2006@hotmail.com>
2025-09-04 04:40:38 +0300

    handle expected error

--- cos_sim.py
+++ cos_sim.py
@@ -14,37 +14,39 @@ movies = {
     '12 Angry Men':[9.6,2.5,3,5.5,5.8,3.5],
     'Inception':[8.5,9,9.4,6,6.8,7.5],
     'The Matrix':[8,9.5,9.6,6,6.5,7.2],
-    'It\'s a Wonderful Life':[9.2,4.5,6.5,7.8,4,8.5],
+    'It\'s A Wonderful Life':[9.2,4.5,6.5,7.8,4,8.5],
     'Captain America: Brave New World':[5,7,6,5,2,3],
-    'Heads of State':[4,8,1,7,1,2],
+    'Heads Of State':[4,8,1,7,1,2],
     'Sikandar':[4,6,1,2,1,3],
     'Get Out':[8,5,2,6,9,3],
     'Titanic':[9,5,1,2,2,10],
     'Infinity War':[6,9,9,3,2,3],
     'Avatar':[7,9,10,3,2,4],
     'The Babadook':[9,3,1,2,9,2],
-    'The Shape of Water':[8,4,7,3,2,8],
+    'The Shape Of Water':[8,4,7,3,2,8],
     'Everything Everywhere All At Once':[8,7,7,7,3,6],
     'Bird Box':[6,5,3,1,7,2],
     'Parasite':[9,3,2,6,5,4],
     'Moonlight':[9,1,1,2,1,6],
     'Sinners':[8,4,3,2,6,3]
 }
+while True:
+    print("Available movies:")
+    for movie in movies.keys():
+        print("-", movie)
 
-print("Available movies:")
-for movie in movies.keys():
-    print("-", movie)
+    user_movie = input("\nChoose a movie from the list: ".strip()).title()
 
-user_movie = input("\nChoose a movie from the list: ")
+    if user_movie not in movies:
+        print('Movie not found')
 
-if user_movie not in movies:
-    print("Movie not found.")
-else:
-    similarities = {}
-    for movie, ratings in movies.items():
-        if movie != user_movie:
-            similarities[movie] = cosine_similarity(movies[user_movie], ratings)
+    else:
+        similarities = {}
+        for movie, ratings in movies.items():
+            if movie != user_movie:
+                similarities[movie] = cosine_similarity(movies[user_movie], ratings)
 
-    recommended = max(similarities, key=similarities.get)
-    print(f"\nRecommended movie: {recommended}")
-    print(f"Cosine similarity: {similarities[recommended]:.4f}")
+        recommended = max(similarities, key=similarities.get)
+        print(f"\nRecommended movie: {recommended}")
+        print(f"Cosine similarity: {similarities[recommended]:.4f}")
+        break
